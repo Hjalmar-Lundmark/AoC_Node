ed1af0c6e5a02cff115be903285dc9825f0df2b9
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2022-12-02 23:15:46 +0100

    Aoc dec2 complete

--- index.js
+++ index.js
@@ -6,9 +6,49 @@ const data = fs.readFileSync(path.join(__dirname, 'input02.txt'), 'utf-8');
 let points = 0;
 let pointsP2 = 0;
 let i = 0;
+let line = "";
 
 while(true) {
-
+    line = "";
+    for (let o = 0; o < 4; o ++) {
+        line += data.charAt(i);
+        i++;
+    }
+    
+    if (line.includes("A")) {
+        if (line.includes("X")) {
+            points += 4;        //part1
+            pointsP2 += 3;      //part2
+        } else if (line.includes("Y")) {
+            points += 8;
+            pointsP2 += (1+3);
+        } else {
+            points += 3;
+            pointsP2 += (2+6);
+        }
+    } else if (line.includes("B")) {
+        if (line.includes("X")) {
+            points += 1;
+            pointsP2 += 1;
+        } else if (line.includes("Y")) {
+            points += 5;
+            pointsP2 += (2+3);
+        } else {
+            points += 9;
+            pointsP2 += (3+6);
+        }
+    } else {
+        if (line.includes("X")) {
+            points += 7;
+            pointsP2 += 2;
+        } else if (line.includes("Y")) {
+            points += 2;
+            pointsP2 += (3+3);
+        } else {
+            points += 6;
+            pointsP2 += (1+6);
+        }
+    }
 
     if (i >= data.length) {
         break;
@@ -17,4 +57,3 @@ while(true) {
 
 console.log(points);
 console.log(pointsP2);
-console.log(data);
